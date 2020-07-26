1b09ba89ec15ba43a0496502dfe436e684fc0be4
junya-gera <aa106921@gmail.com>
2020-07-26 20:15:14 +0900

    section6 バリデーション　型の指定

--- udemy-vuejs/src/components/LikeNumber.vue
+++ udemy-vuejs/src/components/LikeNumber.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <p>いいね({{ totalNumber / 2 }})</p>
+    <p>いいね({{ halfNumber }})</p>
     <button @click="increment"> +1</button>
   </div>
 </template>
 
 <script>
 export default { // この中にコンポーネントのオブジェクト(dataとかcomputedとか)を記述する
-  props: ["totalNumber"], // 使いたい親コンポーネントのデータをpropsの配列で持つ
+  // props: ["totalNumber"], 使いたい親コンポーネントのデータをpropsの配列、または下のようなオブジェクトで持つ
+  props: {
+    totalNumber: {
+      type: Number, // バリデーション 型が数値じゃなければエラーが出る
+      default: 10 // 何も入力されていなかった場合の値
+      // required: true  この値がなければエラー。defaultがある場合はいらない
+    }
+  },
+  computed: {
+    halfNumber() {
+      return this.totalNumber / 2;
+    }
+  },
   methods: {
     increment: function(){
       this.totalNumber++;
