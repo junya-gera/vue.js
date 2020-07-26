fe1abefc6041837a3b885c3c7fdf7834542af712
junya-gera <aa106921@gmail.com>
2020-07-26 16:59:22 +0900

    section6 .vueファイルをオブジェクトにしてimport

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' 
+// App.vueというコンポーネントの3つ(template,script,style)をAppというオブジェクト(dataとかcomputedとか)にしてimportする
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App), // render関数でAppというオブジェクト(元はコンポーネント)を仮想ノードにしている
 }).$mount('#app')
